b030862b2e543ca4947f0a7c77a9f8073db6dfe6
Lenovo\timothy <timothy0336@hotmail.com>
2020-05-05 11:39:24 +0800

    Add namespace

--- code/networkit_profile.py
+++ code/networkit_profile.py
@@ -1,42 +1,43 @@
-from networkit import *
+import networkit as nk
 from benchmark import benchmark
 import sys
 
 filename = sys.argv[1]
 n = int(sys.argv[2])
 
-engineering.setNumberOfThreads(16)
+nk.engineering.setNumberOfThreads(16)
 print(f"Profiling dataset {filename}")
 
 print("Profiling loading")
 print("=================")
 print()
 
-benchmark("readGraph(filename, Format.SNAP)", globals=globals(), n=n)
-g = readGraph(filename, Format.SNAP)
+benchmark("nk.readGraph(filename, nk.Format.SNAP)", globals=globals(), n=n)
+g = nk.readGraph(filename, nk.Format.SNAP)
 
 print("Profiling shortest path")
 print("=======================")
 print()
 
-benchmark("distance.BFS(g, 0, storePaths=False).run()", globals=globals(), n=n)
+benchmark("nk.distance.BFS(g, 0, storePaths=False).run()",
+          globals=globals(), n=n)
 
 print("Profiling PageRank")
 print("==================")
 print()
 
-benchmark("centrality.PageRank(g, damp=0.85, tol=1e-3).run()",
+benchmark("nk.centrality.PageRank(g, damp=0.85, tol=1e-3).run()",
           globals=globals(), n=n)
 
 print("Profiling k-core")
 print("================")
 print()
 
-benchmark("centrality.CoreDecomposition(g).run()", globals=globals(), n=n)
+benchmark("nk.centrality.CoreDecomposition(g).run()", globals=globals(), n=n)
 
 print("Profiling strongly connected components")
 print("=======================================")
 print()
 
-benchmark("components.StronglyConnectedComponents(g).run()",
+benchmark("nk.components.StronglyConnectedComponents(g).run()",
           globals=globals(), n=n)
